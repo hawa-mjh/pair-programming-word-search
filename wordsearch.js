57001ee06dc4d0f0ed1dc81160a7163d9edd769f
Hawa <hawa.mjh@gmail.com>
2023-05-04 01:44:36 +0000

    Word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) return false; // if letters is an empty array, return false
+  
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+      if (l.includes(word)) return true;
     }
-}
-
-module.exports = wordSearch
+  
+    const verticalJoin = [];
+    for (let i = 0; i < letters[0].length; i++) {
+      let tempVerticalString = "";
+      for (let j = 0; j < letters.length; j++) {
+        tempVerticalString += letters[j][i];
+      }
+      verticalJoin.push(tempVerticalString);
+    }
+    for (const l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+  
+    return false;
+  };
+  
+  module.exports = wordSearch;
